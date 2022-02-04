060ceb4eb3f36e13ba67eda79fc4568d3c655936
Ronan Rocha <ronan.rochasouza@gmail.com>
2022-02-04 17:07:46 -0300

    ajustes

--- server/server.js
+++ server/server.js
@@ -1,24 +1,29 @@
 const express = require('express');
-const routes  = require('./route/main.route');
+const cors = require('cors');
+const routes = require('./route/main.route');
+
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use(function(error, req,res, next) {
+
+app.use(function (error, req, res, next) {
+
 
     console.log(error);
-    if(error.message === 'Task not found') {
-         return res.status(404).send(error.message);
+    if (error.message === 'Task not found') {
+        return res.status(404).json({status: 404, message:error.message});
     }
 
-    if(error.message === 'Task already exists') {
-        return  res.status(409).send(error.message);
+    if (error.message === 'Task already exists') {
+        return res.status(409).json({status: 409, message: error.message});
     }
 
-    res.status(500).send(error.message);
-    
+    res.status(500).json({status: 500, message:error.message});
+
 });
 
 
